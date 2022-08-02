7c22dc726947637196f0cfdb1086d7b5000961d8
flos06 <floriset@gmail.com>
2022-08-02 16:02:20 +0200

    complete practice week 05 timing benchmarks

--- practice-for-week-05-timing-benchmarks-practice/phase-2.js
+++ practice-for-week-05-timing-benchmarks-practice/phase-2.js
@@ -3,13 +3,26 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
-
+  let array =[]
+  for (let i = increment; i <= 10 * increment; i += increment){
+    array.push(addNums(i))
+  }
+  return array;
 }
 
+
+console.log(addNums10(10))
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
 
+  let newArray =[]
+  for (let i = increment; i <= 10 * increment; i += increment){
+    newArray.push(addManyNums(i))
+  }
+  return newArray;
 }
+
+console.log(addManyNums10(10))
 
 module.exports = [addNums10, addManyNums10];
--- practice-for-week-05-timing-benchmarks-practice/phase-3.js
+++ practice-for-week-05-timing-benchmarks-practice/phase-3.js
@@ -1,21 +1,56 @@
 const [addNums, addManyNums] = require("./phase-1");
 
+
+// Runs `addManyNums` in 10 increasing increments
+
+
 function addNums10Timing(increment) {
   // Copy your `addNums10` code here
-  // Then, add timing code
 
-  // Your code here
+
+
+    let array =[]
+    for (let i = increment; i <= 10 * increment; i += increment){
+      let startTime =Date.now()
+      array.push(addNums(i))
+      let endTime = Date.now()
+      console.log(endTime - startTime)
+      console.time()
+      console.timeLog()
+      console.timeEnd()
+      
+    }
+    return array;
+  
+  
 
 }
+
+console.log(addNums10Timing(10))
 
 
 function addManyNums10Timing(increment) {
 // Copy your `addManyNums10` code here
-// Then, add timing code
 
-  // Your code here
 
+
+
+  let newArray =[]
+  for (let i = increment; i <= 10 * increment; i += increment){
+    
+
+    let startTime =Date.now()
+    newArray.push(addManyNums(i))
+    let endTime = Date.now()
+    console.log(endTime - startTime)
+    console.time()
+    console.timeLog()
+    console.timeEnd()
+  }
+  return newArray;
 }
+
+
 
 
 n = 1000000
