5e8baccb5313ca648212da6da89986ab9605395e
flos06 <floriset@gmail.com>
2022-08-02 15:36:59 +0200

    fix phase 1

--- practice-for-week-05-timing-benchmarks-practice/phase-1.js
+++ practice-for-week-05-timing-benchmarks-practice/phase-1.js
@@ -1,15 +1,24 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
   // Fill this in
-
+  let sum = 0
+  for (let i = 0; i <= n; i++){
+    sum += i;
+  }
+  return sum;
 }
-
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
   // Fill this in
+  let manysum = 0;
+  for (let i = 0; i <= n; i++){
+    manysum += addNums(i);
+  }
+  return manysum;
 
 }
+
 
 
 
